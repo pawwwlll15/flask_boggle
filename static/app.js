a735662878bce772e239e3242c22d75e8e05a60c
Paul Mozeley <pdreier15@gmail.com>
2024-09-25 13:06:21 -0400

    incorporating jquery

--- static/app.js
+++ static/app.js
@@ -1,51 +1,52 @@
+$(document).ready(function(){
 
-/* listen for when the page loads, when the happens grab form with id 'guess' and add an event listener to that form as well */
-const guess_form = document.querySelector('#guess');
-let used_guesses = new Set();
-let score = document.querySelector('#score');
-let counter = 0;
-score.textContent = "Youre Score: " + counter
-let top_score = 0;
-let top_score_h4 = document.querySelector('#top_score');
-top_score_h4.textContent = top_score;
+    /* listen for when the page loads, when the happens grab form with id 'guess' and add an event listener to that form as well */
+    const guess_form = $('#guess');
+    let used_guesses = new Set();
+    let score = $('#score');
+    let counter = 0;
+    score.text("Youre Score: " + counter)
+    let top_score = 0;
+    let top_score_h4 = $('#top_score');
+    top_score_h4.text(top_score);
 
 
 
-guess_form.addEventListener('submit', async function(e){
-    e.preventDefault();
-    /* grab validation message */
-    let validation_message = document.querySelector('#validation_message');
-    /* get value of guess input after submission, then add guessLi to used_guesses so user can keep track of their guesses */
-    let guess_input = document.querySelector('#guess_input').value;
-      
-    /* sending input data to /submit_guess */
-    const response = await axios.post('/submit_guess',{guess_input: guess_input})
-    const data = response.data;
-    /* isolate validation result and either fill in validation message text content or run alert message. */
-    let validation = data.result;
-    if(validation == 'ok'){
-        validation_message.textContent = 'Great find!!'
-        if(used_guesses.has(guess_input)){
-            alert('Sorry you already used this word!');
-            
-        }
-        /* if used_guesses Set does not include guess add it to set and add counter to 1 for player point */
-        else if(!used_guesses.has(guess.input)) {
-            used_guesses.add(guess_input);
-            counter += 1;
-            score.textContent = "Youre Score: " + counter;
+    guess_form.on('submit', async function(e){
+        e.preventDefault();
+        /* grab validation message */
+        let validation_message = $('#validation_message');
+        /* get value of guess input after submission, then add guessLi to used_guesses so user can keep track of their guesses */
+        let guess_input = $('#guess_input').val();
+        
+        /* sending input data to /submit_guess */
+        const response = await axios.post('/submit_guess',{guess_input: guess_input})
+        const data = response.data;
+        /* isolate validation result and either fill in validation message text content or run alert message. */
+        let validation = data.result;
+        if(validation == 'ok'){
+            validation_message.text('Great find!!')
+            if(used_guesses.has(guess_input)){
+                alert('Sorry you already used this word!');
                 
+            }
+            /* if used_guesses Set does not include guess add it to set and add counter to 1 for player point */
+            else if(!used_guesses.has(guess_input)) {
+                used_guesses.add(guess_input);
+                counter += 1;
+                score.text("Youre Score: " + counter);
+                    
+            }
+        } else if(validation == 'not-on-board'){
+            validation_message.text('');
+            alert('Sorry that word is not on the board');
+                
+        } else if(validation == 'not-word'){
+            validation_message.text('');
+            alert('Sorry that is not a word!!');
         }
-    } else if(validation == 'not-on-board'){
-        validation_message.textContent = '';
-        alert('Sorry that word is not on the board');
-            
-    } else if(validation == 'not-word'){
-        validation_message.textContent = '';
-        alert('Sorry that is not a word!!');
-    }
-    
-    this.reset()
+        
+        this.reset()
 
+    })
 })
-
--- static/timer.js
+++ static/timer.js
@@ -1,29 +1,38 @@
-let timeCounter = 60;
-let timer = document.querySelector('#timer');
-timer.textContent = timeCounter;
+   
+$(document).ready(function(){
+
+    let timeCounter = 60;
+    let timer = $('#timer');
+    timer.text(timeCounter);
 
 
 
 
-/* timer that implements -1 every second, when timeCounter hits 0, run alert message and then send player back to home page */
-function updateTimer(){
-    if(timeCounter > 0){
-        timeCounter -= 1;
-        timer.textContent = timeCounter;
-    } else{
-        clearInterval(countdown);
-        alert("TIME IS UP, PLEASE CLICK 'OK' TO HEAD BACK TO HOME PAGE!!");
-        window.location.href = '/';
-        /* counter and top_score are defined in app.js */
-        if(counter > top_score){
-            top_score = counter;
+    /* timer that implements -1 every second, when timeCounter hits 0, run alert message and then send player back to home page */
+    function updateTimer(){
+        if(timeCounter > 0){
+            timeCounter -= 1;
+            timer.text(timeCounter);
+        } else{
+            clearInterval(countdown);
+            alert("TIME IS UP, PLEASE CLICK 'OK' TO HEAD BACK TO HOME PAGE!!");
+            window.location.href = '/';
+            /* counter and top_score are defined in app.js */
+            if(counter > top_score){
+                top_score = counter;
+            }
+            
         }
-        
     }
-}
 
-let countdown = setInterval(updateTimer,1000);
+    let countdown = setInterval(updateTimer,1000);
 
 
-/* top score is not updating for some reason!! */
+    /* top score is not updating for some reason!! */
+
+
+
+
+});
+ 
 
